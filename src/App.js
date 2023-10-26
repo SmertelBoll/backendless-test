131dfbe230dc87efa3161dba3d07548cedc32713
Lyubomyr <sholop2113@gmail.com>
2023-10-26 16:05:44 +0300

    project with static json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import "./App.css";
+import { Suspense, lazy, useEffect, useState } from "react";
+
+const json = [
+  { id: "dummyTable", title: "Dummy Table", order: 1, path: "tabs/dummyTable.js" },
+  { id: "dummyChart", title: "Dummy Chart", order: 2, path: "tabs/dummyChart.js" },
+  { id: "dummyList", title: "Dummy List", order: 0, path: "tabs/dummyList.js" },
+];
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [currentObj, setCurrentObj] = useState(false);
+
+  useEffect(() => {
+    json.sort((a, b) => a.order - b.order);
+
+    const idOfPage = location.pathname.slice(1);
+    if (!idOfPage) {
+      setCurrentObj(json[0]);
+      navigate(json[0].id);
+    }
+  }, []);
+
+  useEffect(() => {
+    const idOfPage = location.pathname.slice(1);
+    const foundObject = json.find((item) => item.id === idOfPage);
+    if (foundObject) {
+      setCurrentObj(foundObject);
+    }
+  }, [location]);
+
+  const getComponent = (path) => {
+    const LazyComponent = lazy(() => import(`./${path}`));
+    return <LazyComponent />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navigate">
+        {json.map((obj) => (
+          <Link to={obj.id} key={obj.id}>
+            {obj.title}
+          </Link>
+        ))}
+      </div>
+      <div className="content">
+        <Routes>
+          <Route
+            path={`/${currentObj.id}`}
+            element={<Suspense fallback={<div>Loading...</div>}>{getComponent(currentObj.path)}</Suspense>}
+          />
+
+          <Route path="/*" element={<div>Page not found..</div>} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/tabs/dummyChart.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function DummyChart() {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>Dummy</td>
+          <td>Chart</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default DummyChart;
